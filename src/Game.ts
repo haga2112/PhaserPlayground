2d1abb829cd90f2aad7ea63aac4176eb1cde9e2c
Vinícius Haga <vinicius.haga@dartdigital.com.br>
2019-06-10 18:07:54 -0300

    Sample game tutorial complete

--- src/Game.ts
+++ src/Game.ts
@@ -23,10 +23,14 @@ function SimpleGame() {
 
 	var game = new Phaser.Game(config);
 
+	// elements
 	var platforms;
 	var player;
 	var cursors;
 	var stars;
+	var score = 0;
+	var scoreText;
+	var bombs;
 
 	function preload() {
 		this.load.image('sky', 'assets/sky.png');
@@ -93,6 +97,14 @@ function SimpleGame() {
 
 		this.physics.add.collider(stars, platforms);
 		this.physics.add.overlap(player, stars, collectStar, null, this);
+	
+		scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+		// bombs
+		bombs = this.physics.add.group();
+
+		this.physics.add.collider(bombs, platforms);
+		this.physics.add.collider(player, bombs, hitBomb, null, this);
 	}
 
 	function update() {
@@ -117,6 +129,31 @@ function SimpleGame() {
 	function collectStar (player, star)
 	{
 		star.disableBody(true, true);
+
+		score += 10;
+		scoreText.setText('Score: ' + score);
+
+		if	(stars.countActive(true) === 0) {
+			stars.children.iterate(function (child) {
+
+				child.enableBody(true, child.x, 0, true, true);
+	
+			});
+	
+			var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+	
+			var bomb = bombs.create(x, 16, 'bomb');
+			bomb.setBounce(1);
+			bomb.setCollideWorldBounds(true);
+			bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+		}
+	}
+
+	function hitBomb(player, bomb) {
+		this.physics.pause();
+		player.setTint(0xff0000);
+		player.anims.play('turn');
+		gameOver = true;
 	}
 }
 
